a19a205800a1e3bcf05a9525601a43836ec35813
Passion-Logan <wangdadamailbox@163.com>
2019-04-02 14:11:42 +0800

    security jwt实现登录鉴权

---
+++ security_jwt_demo/src/main/java/com/cody/SecurityJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtDemoApplication.class, args);
+	}
+
+}
