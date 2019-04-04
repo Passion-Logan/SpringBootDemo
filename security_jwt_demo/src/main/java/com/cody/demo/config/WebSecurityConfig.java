d496489189881ae3ad7151a29b841537cb55b574
Passion-Logan <wangdadamailbox@163.com>
2019-04-04 17:59:37 +0800

    添加securityConfig配置

--- security_jwt_demo/src/main/java/com/cody/demo/config/WebSecurityConfig.java
+++ security_jwt_demo/src/main/java/com/cody/demo/config/WebSecurityConfig.java
@@ -1,11 +1,20 @@
 package com.cody.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @File Name: com.cody.demo.config
@@ -31,8 +40,61 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 
     private final JwtAuthenticationTokenFilter authenticationTokenFilter;
 
+    @Autowired
+    public WebSecurityConfig(JwtAuthenticationEntryPoint unauthorizedHandler,
+                             @Qualifier("RestAuthenticationAccessDeniedHandler") AccessDeniedHandler accessDeniedHandler,
+                             @Qualifier("CustomUserDetailsService") UserDetailsService CustomUserDetailsService,
+                             JwtAuthenticationTokenFilter authenticationTokenFilter) {
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.CustomUserDetailsService = CustomUserDetailsService;
+        this.authenticationTokenFilter = authenticationTokenFilter;
+    }
 
+    @Autowired
+    public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception
+    {
+        authenticationManagerBuilder
+                // 设置设置UserDetailsService
+                .userDetailsService(this.CustomUserDetailsService)
+                // 使用BCrypt进行密码的hash
+                .passwordEncoder(passwordEncoder());
+    }
 
+    /**
+     * 装载BCrypt密码编码器
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler).and()
+                // 由于使用的是JWT，我们这里不需要csrf
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                // 基于token，所以不需要session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+                .authorizeRequests()
+
+                // 对于获取token的rest api要允许匿名访问
+                .antMatchers("/api/demo/login", "/api/demo/sign", "/error/**").permitAll()
+                // 除上面外的所有请求全部需要鉴权认证
+                .anyRequest().authenticated();
+
+        // 禁用缓存
+        http.headers().cacheControl();
+        // 添加JWT filter
+        http.addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
 
 }
