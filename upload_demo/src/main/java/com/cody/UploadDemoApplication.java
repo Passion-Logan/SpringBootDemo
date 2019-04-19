f214068e3607c47563fa268949f3844e2df79776
Passion-Logan <wangdadamailbox@163.com>
2019-04-19 11:44:54 +0800

    提交修改

--- upload_demo/src/main/java/com/cody/UploadDemoApplication.java
+++ upload_demo/src/main/java/com/cody/UploadDemoApplication.java
@@ -1,7 +1,11 @@
 package com.cody;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UploadDemoApplication {
@@ -10,4 +14,22 @@ public class UploadDemoApplication {
 		SpringApplication.run(UploadDemoApplication.class, args);
 	}
 
+    /**
+     * 解决上传文件大于10M出现连接重置问题，此异常内容GlobalException也捕获不到。
+     *
+     * @return
+     */
+	@Bean
+	public TomcatServletWebServerFactory tomcatEmbedded()
+    {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connecter -> {
+            if ((connecter.getProtocolHandler() instanceof AbstractHttp11Protocol<?>))
+            {
+                ((AbstractHttp11Protocol) connecter.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+    }
 }
