6058fc437653db5dd3b2e4e652afe9fc94dc4164
Passion-Logan <wangdadamailbox@163.com>
2019-04-19 10:45:45 +0800

    添加上传文件Demo

---
+++ upload_demo/src/main/java/com/cody/UploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDemoApplication.class, args);
+	}
+
+}
