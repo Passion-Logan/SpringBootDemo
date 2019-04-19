0265f1f1ed75830ebc8b39fbceb82b6600cdcf4e
Passion-Logan <wangdadamailbox@163.com>
2019-04-19 17:07:37 +0800

    添加文件上传Demo

--- upload_demo/src/main/java/com/cody/controller/demoController.java
+++ upload_demo/src/main/java/com/cody/controller/demoController.java
@@ -1,5 +1,17 @@
 package com.cody.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @File Name: com.cody.controller
  * @Author: WQL //作者及
@@ -10,7 +22,42 @@ package com.cody.controller;
  * @Others: // 其它内容的说明
  * @History: // 历史修改记录
  */
+@Controller
 public class demoController
 {
+
+    private static String UPLOADED_FOLDER = "C://Users//user//Desktop//upload//";
+
+    @RequestMapping("/")
+    public String demo()
+    {
+        return "demo";
+    }
+
+    @PostMapping("upload")
+    public String upload(@RequestParam(name = "file")MultipartFile file, RedirectAttributes redirectAttributes)
+    {
+        if (file.isEmpty())
+        {
+            redirectAttributes.addFlashAttribute("message", "请选择文件");
+            return "redirect:/";
+        }
+
+        try
+        {
+            byte[] bytes = file.getBytes();
+
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "成功上传 '" + file.getOriginalFilename() + "'");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return "redirect:/";
+    }
 
 }
---
+++ upload_demo/src/main/java/com/cody/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.cody.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * @File Name: com.cody.exception
+ * @Author: WQL //作者及
+ * @Date: 2019/4/19 17:03//完成日期
+ * @Description: // 描述
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler
+{
+
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "redirect:/uploadStatus";
+    }
+
+}
