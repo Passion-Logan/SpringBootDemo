e079707bee8bba2a9842963d188b4aa2f9404b28
Passion-Logan <wangdadamailbox@163.com>
2019-06-25 15:50:46 +0800

    添加optional

---
+++ Java_8/src/newFunction/optional.java
@@ -0,0 +1,16 @@
+package newFunction;
+
+/**
+ * @File Name: newFunction
+ * @Author: WQL //作者及
+ * @Date: 2019/6/25 15:49//完成日期
+ * @Description: // 描述
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class optional
+{
+
+}
