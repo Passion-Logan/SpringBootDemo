6fc810ed9c5a77fcbd06678b561eb1b1e72d236d
Passion-Logan <wangdadamailbox@163.com>
2019-07-18 14:36:12 +0800

    添加QuartZ 定时任务控制Demo

---
+++ quartz_plus/src/main/java/com/demo/cody/quartz_plus/QuartzPlusApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cody.quartz_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzPlusApplication.class, args);
+	}
+
+}
