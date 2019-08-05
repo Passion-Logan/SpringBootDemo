fea90146cf20a00271c21376a0cfdb02e0f9a8ff
Passion-Logan <wangdadamailbox@163.com>
2019-08-05 18:03:15 +0800

    添加定时任务执行类

---
+++ quartz_plus/src/main/java/com/demo/cody/utils/SpringContextUtils.java
@@ -0,0 +1,76 @@
+package com.demo.cody.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.Objects;
+
+/**
+ * @File Name: com.demo.cody.utils
+ * @Author: WQL //作者及
+ * @Date: 2019/8/5 17:49//完成日期
+ * @Description: // spring获取bean工具类
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class SpringContextUtils implements ApplicationContextAware
+{
+
+    private static ApplicationContext applicationContext = null;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        if (SpringContextUtils.applicationContext == null)
+        {
+            SpringContextUtils.applicationContext = applicationContext;
+        }
+    }
+
+    /**
+     * 获取applicationContext
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean
+     *
+      * @param name
+     * @return
+     */
+    public static Object getBean(String name)
+    {
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz)
+    {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
