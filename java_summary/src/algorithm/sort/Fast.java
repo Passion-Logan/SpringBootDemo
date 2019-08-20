412ab1f511bcbc856ef0b585fb4b088b195ef994
Passion-Logan <wangdadamailbox@163.com>
2019-08-20 20:03:52 +0800

    添加快速排序

---
+++ java_summary/src/algorithm/sort/Fast.java
@@ -0,0 +1,15 @@
+package algorithm.sort;
+
+/**
+ * @File Name: algorithm.sort
+ * @Author: WQL //作者及
+ * @Date: 2019/8/20 20:03//完成日期
+ * @Description: // 快速排序
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class Fast
+{
+}
