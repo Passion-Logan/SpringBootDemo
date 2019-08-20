73dc51745473f1e6f4b955bc398beb4b0a45ebd6
Passion-Logan <wangdadamailbox@163.com>
2019-08-20 16:47:21 +0800

    添加修改

---
+++ java_summary/src/algorithm/sort/Bubbling.java
@@ -0,0 +1,40 @@
+package algorithm.sort;
+
+/**
+ * @File Name: algorithm.Bubbling
+ * @Author: WQL //作者及
+ * @Date: 2019/8/20 15:36//完成日期
+ * @Description: // 冒泡排序
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class Bubbling
+{
+}
+
+class sort {
+    /**
+     * 冒泡排序(大的值从前往后冒泡)
+     *  优点：稳定排序，适用于数组存储的数据和链表存储的数据
+     * @param a
+     * @return
+     */
+    public static int[] bubbleSort(int[] a) {
+        for (int end = a.length - 1; end > 0; end--) {
+            // 增加一个判断是否发生过交换的标记
+            boolean flag = false;
+        }
+    }
+
+    /**
+     * 冒泡排序(小的值从后往前下沉)
+     *  优点：稳定排序，适用于数组存储的数据和链表存储的数据
+     * @param a
+     * @return
+     */
+    public static int[] bubbleSort2(int[] a) {
+
+    }
+}
