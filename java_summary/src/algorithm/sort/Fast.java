2b09f08b2c73443975c2fcf3e7882c41992fa370
Passion-Logan <wangdadamailbox@163.com>
2019-08-20 21:21:43 +0800

    添加快速排序

--- java_summary/src/algorithm/sort/Fast.java
+++ java_summary/src/algorithm/sort/Fast.java
@@ -1,5 +1,7 @@
 package algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * @File Name: algorithm.sort
  * @Author: WQL //作者及
@@ -12,4 +14,47 @@ package algorithm.sort;
  */
 public class Fast
 {
+
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{1,6,3,5,2};
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(quickSort(arr)));
+    }
+
+    public static int[] quickSort(int[] a) {
+        if (a.length > 0) {
+            quickSortRecursion(a,0, a.length - 1);
+        }
+        return a;
+    }
+
+    public static void quickSortRecursion(int[] data, int low, int high) {
+        if (low < high) {
+            int middle = partition(data, low, high);
+            quickSortRecursion(data, low, middle - 1);
+            quickSortRecursion(data, middle + 1, high);
+        }
+    }
+
+    public static int partition(int[] data, int low, int high) {
+        int temp = data[low];
+        while (low < high) {
+            while (low < high && data[high] >= temp) {
+                high--;
+            }
+            // 比中轴小的记录移到低端
+            data[low] = data[high];
+            while (low < high && data[low] <= temp) {
+                low++;
+            }
+            // 比中轴大的记录移到高端
+            data[high] = data[low];
+        }
+        data[low] = temp;
+
+        // 返回中轴位置
+        return low;
+    }
 }
